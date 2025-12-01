6df5ae8aa56fa3630b9443698908c64ea2790d87
Vanshika Sharma <vanshika11082004@gmail.com>
2025-12-02 00:20:25 +0530

    feat: add basic C++ program to input two numbers and print their sum

---
+++ Basics/easy/Sum_of_two_numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter a num: ";
+    cin>>a;
+    cout<<"Enter a num: ";
+    cin>>b;
+    cout<<a+b<<endl;
+    return 0;
+
+}
