65aeaa8a9b0df6324c707581372b456601eb981c
Vanshika Sharma <vanshika11082004@gmail.com>
2025-12-02 01:02:48 +0530

    feat: add loop program to print Hello N times

---
+++ Basics/easy/Print_Hello_n_times.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"enter a num : ";
+    cin>>n;
+    
+
+    for (int i = 0; i < n; i++)
+    {
+        cout<<"Hello\n";
+    }
+    return 0;
+    
+}
