07b5afd3d1c054bf220c5b17da0b85f551ea4423
Vanshika Sharma <vanshika11082004@gmail.com>
2025-12-02 22:38:29 +0530

    feat: add basic input-output function to print a number

---
+++ Basics/easy/funtion_to_print_Number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void printNumber(){
+    int n;
+    cin>> n;
+    cout<<"number is : "<< n;
+}
+
+
+
+int main(){
+    printNumber();
+    return 0;
+
+
+}
