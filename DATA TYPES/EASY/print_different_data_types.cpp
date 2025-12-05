5332bdd632d318ae0c90bf06d12ab62576a7bc70
Vanshika Sharma <vanshika11082004@gmail.com>
2025-12-05 21:36:42 +0530

    feat: add program to input and print different data types

---
+++ DATA TYPES/EASY/print_different_data_types.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num;
+    float f;
+    char ch;
+
+    cout<<"Enter an interger: ";
+    cin>>num;
+
+    cout<<"Enter a float: ";
+    cin>>f;
+
+    cout<<"Enter a character: ";
+    cin>>ch;
+
+
+    cout<<"Integer: "<<num<<endl;
+    cout<<"Float: "<<f<<endl;
+    cout<<"Character: "<<ch<<endl;
+
+
+    return 0;
+
+}
