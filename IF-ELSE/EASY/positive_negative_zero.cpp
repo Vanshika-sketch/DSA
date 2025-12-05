0c78fde5717d2416c4c04df3be3a53d3300252ce
Vanshika Sharma <vanshika11082004@gmail.com>
2025-12-05 22:00:36 +0530

    feat: add positive-negative-zero number checker

---
+++ IF-ELSE/EASY/positive_negative_zero.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter a num: ";
+    cin>>n;
+
+    if (n>0)
+    {
+        cout<<"num is positive";
+    }else if (n<0)
+    {
+        cout<<"num is negative";
+    }else{
+        cout<<"num is zero";
+}
+    
+    
+}
