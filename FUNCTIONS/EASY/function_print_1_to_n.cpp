fd7bcde5f7f30379e7f4195ff4de23b75d8683d7
Vanshika Sharma <vanshika11082004@gmail.com>
2025-12-17 01:49:43 +0530

    feat: add function to print numbers from 1 to n

---
+++ FUNCTIONS/EASY/function_print_1_to_n.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void printNumber(int n){
+    for(int i = 1 ; i <= n;i++){
+        cout<< i << " ";
+    }
+}
+
+
+int main(){
+    printNumber(5);
+
+
+
+    return 0;
+}
