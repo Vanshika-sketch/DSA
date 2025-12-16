8887bb713976ea8c4e6a70899fbb55613d3fa2a9
Vanshika Sharma <vanshika11082004@gmail.com>
2025-12-16 22:18:36 +0530

    feat: implement function that returns sum of two numbers

---
+++ FUNCTIONS/EASY/function_add_two_numbers.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int printNum(int a,int b){
+   return a + b;
+    
+
+}
+
+
+
+int main(){
+    int sum = printNum(5,5);
+    cout<<sum;
+
+    
+
+
+
+
+
+    return 0;
+}
