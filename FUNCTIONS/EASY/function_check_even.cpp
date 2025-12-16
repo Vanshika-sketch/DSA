f438c149611c3c56d9d46e326eb44b5df2497e76
Vanshika Sharma <vanshika11082004@gmail.com>
2025-12-16 22:39:11 +0530

    feat: add function to check even or odd using boolean return

---
+++ FUNCTIONS/EASY/function_check_even.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+
+bool isEven(int num){
+    if(num%2==0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+
+    
+    if(isEven(n))
+        cout << "Even";
+    else
+        cout << "Odd";
+
+
+  
+
+
+
+
+    return 0;
+
+}
