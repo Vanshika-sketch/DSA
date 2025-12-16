25b012112f52aed159f8db87843a8e4de0c5d161
Vanshika Sharma <vanshika11082004@gmail.com>
2025-12-16 13:50:46 +0530

    update :implement function with parameter input

--- FUNCTIONS/EASY/BasicFunction.cpp
+++ FUNCTIONS/EASY/BasicFunction.cpp
@@ -1,11 +1,20 @@
 #include<iostream>
 using namespace std;
+                                  // Function with parameters(input)
+void printNumber(int n) {
+    cout<< n;
+}
+    // n : parameter
+
 void printHello(){
-    cout<<"Hello World";
+    cout<<"\nHello World";
 }
 int main(){
 
-    printHello();
+    int x = 4;
+    printNumber(x ); //x is an argument,  value of x is copied into n
+
+    printHello(); //function call
 
 
     return 0 ;
