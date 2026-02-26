2acb23390ddd41e3a73cf153fb53aa2a9df8d406
Vanshika Sharma <vanshika11082004@gmail.com>
2026-02-26 13:28:07 +0530

    did quextion on array

--- LEETCODE/EASY/Array/01_sum/01_sum.cpp
+++ LEETCODE/EASY/Array/01_sum/01_sum.cpp
@@ -1,42 +1,13 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int main() {
-    int n;
-    cout << "Enter size of array: ";
-    cin >> n;
+    int arr[300];
 
-    vector<int> nums(n);
-    cout << "Enter array elements: ";
-    for (int i = 0; i < n; i++) {
-        cin >> nums[i];
+    for(int i = 0; i < 300; i++) {
+        arr[i] = 24;
     }
 
-    int target;
-    cout << "Enter target sum: ";
-    cin >> target;
-
-    vector<int> res;
-    bool found = false;
-
-    for (int i = 0; i < nums.size(); i++) {
-        for (int j = i + 1; j < nums.size(); j++) {
-            if (nums[i] + nums[j] == target) {
-                res.push_back(i);
-                res.push_back(j);
-                found = true;
-                break;
-            }
-        }
-        if (found) break;
-    }
-
-    if (found) {
-        cout << "Indices: " << res[0] << " " << res[1];
-    } else {
-        cout << "No valid pair found";
-    }
-
+    cout << arr[45];   // will print 24
     return 0;
 }
