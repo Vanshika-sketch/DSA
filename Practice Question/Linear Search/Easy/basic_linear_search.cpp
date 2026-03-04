baccfa4e75ffa55f047d643a189d7a9d5d3d3d28
Vanshika Sharma <vanshika11082004@gmail.com>
2026-03-04 16:39:13 +0530

    feat: implement linear search algorithm with basic test cases

---
+++ Practice Question/Linear Search/Easy/basic_linear_search.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+bool search(int arr[],int size,int key){
+    for (int i = 0; i < size ; i++)
+    {
+        if(arr[i]==key){
+            return 1;
+        }
+       
+    }
+    return 0;
+    
+
+    
+}
+
+
+int main(){
+    int arr[10]={10,9,8,7,4,5,6,3,2,1};
+    cout<<"Enter the element to search for: "<<endl;
+    int key;
+    cin>>key;
+
+    bool found = search(arr,10,key);
+    if(found){
+        cout<<"key is present"<<endl;
+    }
+    else{
+        cout<<"key is not present"<<endl;
+    }
+
+
+
+
+
+    return 0;
+
+}
