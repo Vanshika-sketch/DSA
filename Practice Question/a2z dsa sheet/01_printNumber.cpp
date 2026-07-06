7a9a0c5e0d28e900c614ef6ff282c3c0ed068462
vanshika <vanshika11082004@gmail.com>
2026-07-06 13:12:20 +0530

    Add switch statement practice, clean up old files

---
+++ Practice Question/a2z dsa sheet/01_printNumber.cpp
@@ -0,0 +1,15 @@
+/* Complete the function printNumber which takes an integer input from the user and prints it on the screen. */
+
+#include <iostream>
+using namespace std;
+
+void printNumber(int num) {
+    cout << num;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    printNumber(n);
+    return 0;
+}
